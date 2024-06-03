efea977458c9c2d7d887ba5be63c02c9bab936bd
zachary oliver <zspoliver@gmail.com>
2024-06-03 15:48:42 -0400

    adds p4 routing and views lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template_string('<h1>Python Operations with Flask Routing and Views</h1>')
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)
+    return text
+
+@app.route('/count/<int:number>')
+def count(number):
+    numbers = range(number)
+    return '\n'.join(str(num) for num in numbers) + '\n'
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        return 'Invalid operation'
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
